fa479c1000888c4e2ca6d0f8c055485a62e1d750
Robert Vojta <rvojta@me.com>
2019-10-01 14:58:21 +0200

    Update TODO comment
    Signed-off-by: Robert Vojta <rvojta@me.com>

--- src/input.rs
+++ src/input.rs
@@ -3,8 +3,8 @@
 
 use crossterm_utils::Result;
 
-// TODO Do not reexport and create a new common AsyncReader structure in the same way as we
-//      with the TerminalCursor, TerminalInput, ...
+// TODO Create a new common AsyncReader structure (like TerminalCursor, TerminalInput, ...).
+//      To avoid copy & pasting of the documentation, to sync the code organization, ...
 #[cfg(unix)]
 pub use self::unix::{AsyncReader, SyncReader};
 #[cfg(windows)]
